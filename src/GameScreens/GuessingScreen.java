11a80f317ac60b31d2b93aed010d6ab493cf9357
ngager <ngager23@gmail.com>
2015-04-17 15:40:38 -0400

    Need to fix the randomization

--- src/GameScreens/GuessingScreen.java
+++ src/GameScreens/GuessingScreen.java
@@ -1,5 +1,8 @@
 package GameScreens;
 
+import Objects.GuessObjects;
+import Objects.ScanObjects;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -9,6 +12,8 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Created by danny on 2/26/15.
@@ -18,14 +23,82 @@ public class GuessingScreen extends JPanel implements MouseListener{
     Image background = Toolkit.getDefaultToolkit().createImage(getClass().getClassLoader().getResource("hurricane_template_guessing.png"));
     CardLayout cl;
     JPanel panelContainer;
-    int imageX = 1295;
-    int image1Y = 162, image2Y = 380, image3Y = 598, image4Y = 816;
+    int imageX = 1295 + 10;
+    int image1Y = 162 + 10, image2Y = 380 + 10, image3Y = 598 + 10, image4Y = 816 + 10;
+    private final int NUM_OBJECTS = 5;
+    BufferedImage leftScan;
+    GuessObjects guesses[];
+    int indeces[];
+
+    String objectNames[] = { "dock", "house", "nets", "sailboat", "schoolbus", "tanker", "telpole", "train", "tree", "truck" };
+    GuessObjects guessObjects[];
+    ScanObjects scanObjects[];
+
     public GuessingScreen(CardLayout cl, JPanel panelContainer) {
-        addMouseListener( this );
-        this.setLayout( null );
+        addMouseListener(this);
+        this.setLayout(null);
         this.cl = cl;
         this.panelContainer = panelContainer;
 
+        // Set up all of the game objects
+        guesses = new GuessObjects[4];
+        indeces = new int[4];
+        guessObjects = new GuessObjects[10];
+        scanObjects = new ScanObjects[10];
+        int i = 0;
+        for( String s : objectNames ){
+            guessObjects[i] = new GuessObjects(s);
+            scanObjects[i] = new ScanObjects(s);
+            i++;
+        }
+        System.out.println("here");
+        randomizeObjects();
+    }
+
+    public void randomizeObjects(){
+        Random rand = new Random();
+        int index, i = 0;
+        while( true ){
+            System.out.println( "i: " + i);
+            index = rand.nextInt(10);
+            if( !Arrays.asList(indeces).contains(index) ){
+                indeces[i] = index;
+                i++;
+                if( i == 4 ) break;
+            }
+        }
+        String name  = objectNames[indeces[0]];
+        String name2 = objectNames[indeces[1]];
+        String name3 = objectNames[indeces[2]];
+        String name4 = objectNames[indeces[3]];
+
+        for( int j : indeces )
+            System.out.println( j );
+
+        Random places = new Random();
+
+        // Pick the object for the left
+        for(ScanObjects s : scanObjects){
+            if(s.getName().equals(name)){
+                leftScan = s.img;
+            }
+        }
+        // Pick the winning object for the right
+        for(GuessObjects g : guessObjects ) {
+            if (g.getName().equals(name)) {
+                int place = places.nextInt(4);
+                guesses[place] = g; //TODO repeating numbers here
+            } else if (g.getName().equals(name2)) {
+                int place = places.nextInt(4);
+                guesses[place] = g;
+            } else if (g.getName().equals(name3)) {
+                int place = places.nextInt(4);
+                guesses[place] = g;
+            } else if (g.getName().equals(name4)) {
+                int place = places.nextInt(4);
+                guesses[place] = g;
+            }
+        }
     }
 
     // Very fast and easy way to set a background image
@@ -33,6 +106,27 @@ public class GuessingScreen extends JPanel implements MouseListener{
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+        for( int i = 0; i < guesses.length; i++ ){
+            if( guesses[i] != null ) {
+                switch(i){
+                    case 0:
+                        g.drawImage(guesses[i].img, imageX, image1Y, guesses[i].width, guesses[i].height, this);
+                        break;
+                    case 1:
+                        g.drawImage(guesses[i].img, imageX, image2Y, guesses[i].width, guesses[i].height, this);
+                        break;
+                    case 2:
+                        g.drawImage(guesses[i].img, imageX, image3Y, guesses[i].width, guesses[i].height, this);
+                        break;
+                    case 3:
+                        g.drawImage(guesses[i].img, imageX, image4Y, guesses[i].width, guesses[i].height, this);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        g.drawImage( leftScan, 500, 500, 400, 400, this);
     }
 
     // Don't even need to check mouse click coordinates because the buttons take care of it
---
+++ src/Objects/GuessObjects.java
@@ -0,0 +1,42 @@
+package Objects;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by nicolegager on 4/17/15.
+ */
+public class GuessObjects {
+
+    private String IMG_PATH;
+    private String name;
+    public BufferedImage img;
+    public int x, y, height, width;
+    public Rectangle bounds;
+
+    public GuessObjects(String name){
+
+        this.name = name;
+        IMG_PATH = getClass().getClassLoader().getResource(name + "_sm.jpg").getPath();
+        try {
+            img = ImageIO.read(new File(IMG_PATH));
+            width = img.getWidth();
+            height = img.getHeight();
+//            x = xLoc;
+//            y = yLoc;
+            bounds = new Rectangle( x, y, width, height );
+            bounds.setBounds( x, y, width, height );
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
